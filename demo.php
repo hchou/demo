e7a4bebf446ca121a3c2587b3fd3138a7ce3cc9d
Hans H Chou <hchou@fas.harvard.edu>
2013-09-19 21:40:50 -0400

    initial adds

---
+++ demo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require_once('day-or-night.php'); ?>
+<style type='text/css'>
+       body {
+           background-color:<?=$backgroundColor?>;
+           color:<?=$color?>;
+        }
+</style>
+
+</head>
+
+<body>
+    <?=$backgroundColor?>
+</body>
+</html>
