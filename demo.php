66b3628ff0522670702fa63767c19e5146e7d778
Hans H Chou <hchou@fas.harvard.edu>
2013-09-22 11:25:23 -0400

    modifying indents

--- demo.php
+++ demo.php
@@ -3,10 +3,10 @@
 <head>
     <?php require_once('day-or-night.php'); ?>
 <style type='text/css'>
-       body {
-           background-color:<?=$backgroundColor?>;
-           color:<?=$color?>;
-        }
+    body {
+        background-color:<?=$backgroundColor?>;
+        color:<?=$color?>;
+    }
 </style>
 
 </head>
